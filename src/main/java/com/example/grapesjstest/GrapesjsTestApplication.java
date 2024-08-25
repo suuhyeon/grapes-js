12e1ee56b22ab8e98d042f36bb3e23f3e1be987f
suucode <limmm.mary@gmail.com>
2024-08-25 23:01:39 +0900

    Style: setting grapesjs

---
+++ src/main/java/com/example/grapesjstest/GrapesjsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.grapesjstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapesjsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrapesjsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grapesjstest/GrapesjsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grapesjstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapesjsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
